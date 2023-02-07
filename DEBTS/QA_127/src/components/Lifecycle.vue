2b945f2070fa0fa98c2ca822d1213489fe80a9ce
“Evgeny <ahotickkk@mail.ru>
2023-02-07 09:57:15 +0300

    setup answer

---
+++ DEBTS/QA_127/src/components/Lifecycle.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>{{ info }}</h2>
+  <button @click="change">change address</button>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+
+const info = ref({
+  name: 'evgeny',
+  address: {
+    street: 'any'
+  }
+})
+
+// вотчер не отработает т.к меняется глубокое св-во объекта
+// watch(info, (newValueCount, oldValueCount) => {
+//   console.log('watch');
+// })
+
+// если добавляем deep true, то вотчер сработает
+watch(info, (newValueCount, oldValueCount) => {
+  console.log('watch');
+}, { deep: true } )
+
+const change = () => {
+  info.value.address.street = 'pushkina'
+}
+
+
+</script>
---
+++ DEBTS/QA_127/src/components/Setup.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+/* Setup это синтаксический сахар для использования Composition API внутри однофайловых компонентов (SFC)
+   код внутри компилируется как содержимое функции setup()
+   в отличии от обычного <script>, который выполняется только 1 раз при первом импорте компонента, 
+   код внутри setup будет выполнятся каждый раз, при создании экземпляра компонента
+*/
+
+// Мы можем легко испортировать различные вспомогательные фун-и не разворачивая их в через опцию методов
+import { capitalize } from './helpers'
+
+
+const msg: string = 'Hello!'
+
+const printAny = () => {
+    console.log(msg)
+}
+
+</script>
+
+<template>
+  <button @click="printAny">{{ msg }}</button>
+  <div>{{ capitalize('hello') }}</div>
+</template>
